9a5f39aab63b61e8a35b19badb4a184d7d8625d0
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-06-09 12:33:04 -0500

    fix: key

--- server/services/ballDontLie.ts
+++ server/services/ballDontLie.ts
@@ -1,7 +1,7 @@
 // these aren't in the global config to keep them self contained
 // as a new vendor may not need the same inputs for a request
 const ENDPOINT = "https://api.balldontlie.io/v1/teams";
-const KEY = "6aec21af-5b8b-4b7a-ad72-6087073a89f4"; // (process.env.NBA_KEY || "").trim();
+const KEY = (process.env.NBA_KEY || "").trim();
 
 export async function getTeams() {
   const response = await fetch(ENDPOINT, {
